a66071819dfe88fc56fea2d12c98f5eec2691546
Povilas Balciunas <balciunas90@gmail.com>
2016-06-14 18:43:17 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='fnv',
+    version='0.1.0',
+    description='FNV hash functions.',
+    long_description=open('README.rst').read(),
+    url='https://github.com/povilasb/pyfnv',
+    author='Povilas Balciunas',
+    author_email='balciunas90@gmail.com',
+    license='MIT',
+    packages=['fnv'],
+    zip_safe=False
+)
